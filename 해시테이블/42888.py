0918d735841de1bde631e103f2befa81b14e147f
lunacircle4 <lunacircle4@gmail.com>
2021-12-21 22:42:04 +0900

    feat: 해시테이블 42888

--- 해시테이블/42888.py
+++ 해시테이블/42888.py
@@ -1,19 +1,15 @@
 def solution(record):
-    user_pool = dict()
-    log_pool = {
-        "Enter": "님이 들어왔습니다.",
-        "Leave": "님이 나갔습니다."
-    }
-    logs = []
-    
+    answer = []
+    names = {}
+    printer = {'Enter':'님이 들어왔습니다.', 'Leave':'님이 나갔습니다.'}
+
     for _record in record:
-        command, uid = _record[0], _record[1]
-        if command == "Change":
-            user_pool.update({uid: _record[2]})
-        else:
-            logs.append([uid, log_pool[command]])
-    
-    for i in range(len(logs)):
-        logs[i] = user_pool[logs[i][0]] + logs[i][1]
-    
-    return logs
+        _record = _record.split()
+        if _record[0] in ['Enter', 'Change']:
+            names[_record[1]] = _record[2]
+
+    for _record in record:
+        if _record.split()[0] != 'Change':
+            answer.append(names[_record.split()[1]] + printer[_record.split()[0]])
+
+    return answer

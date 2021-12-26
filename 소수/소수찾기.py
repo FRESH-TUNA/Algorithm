e43ba9f5c67aab143b489753c72ea9a813f4a8ec
lunacircle4 <lunacircle4@gmail.com>
2021-12-26 14:12:13 +0900

    feat: 소수찾기/2

--- 소수/소수찾기.py
+++ 소수/소수찾기.py
@@ -1,37 +1,18 @@
-https://programmers.co.kr/learn/courses/30/lessons/42839
+# https://programmers.co.kr/learn/courses/30/lessons/42839
 
-from itertools import permutations
-def solution(n):
-    a = set()
-    for i in range(len(n)):
-        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
-    a -= set(range(0, 2)) 
-
-    for i in range(2, int(max(a) ** 0.5) + 1):
-        a -= set(range(i * 2, max(a) + 1, i))
-    return len(a)
-
-
-####
 from itertools import permutations
 from functools import reduce
 import math
 
-def is_prime_number(x):
-    if x in (0, 1): return False
-    for i in range(2, int(math.sqrt(x)) + 1):
-        if x % i == 0:
-            return False
+def is_prime(num):
+    if num in (0, 1): return False
+    for case in range(2, int(math.sqrt(num)) + 1):
+        if num % case == 0: return False
     return True
 
 def solution(numbers):
-    numbers = list(numbers)
-    answer, cases = 0, set()
-    
+    cases = set()
     for i in range(1, len(numbers) + 1):
-        cases |= set([int("".join(case)) for case in permutations(numbers, i)])
-    
-    for case in cases:
-        if is_prime_number(case): answer += 1
-            
-    return answer
+        cases |= {int(''.join(case)) for case in permutations(numbers, i)}
+    return reduce(
+        lambda acc, case: acc + (1 if is_prime(case) else 0), cases, 0)

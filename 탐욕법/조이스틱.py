15dc62255ce4f485017ee22547e13d8b4656d951
lunacircle4 <lunacircle4@gmail.com>
2021-12-27 12:32:17 +0900

    feat: 조이스틱

--- 탐욕법/조이스틱.py
+++ 탐욕법/조이스틱.py
@@ -1,20 +1,15 @@
-# https://programmers.co.kr/learn/courses/30/lessons/42860
-# https://bellog.tistory.com/152
-
 def solution(name):
-    answer = 0
-    min_move = len(name) - 1
-    next_pos = 0
+    name = list(name)
+    shifted = 0
+    min_traced = len(name) - 1
     
-    for i, char in enumerate(name):
-        answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+    for idx, c in enumerate(name):
+        shifted += min(ord(c) - ord("A"), ord("Z") - ord(c) + 1)    
+        next_A_idx = idx + 1
         
-        # 연속되는 구간
-        next_pos = i + 1
-        while next_pos < len(name) and name[next_pos] == 'A':
-            next_pos += 1
+        while next_A_idx < len(name) and name[next_A_idx] == "A":
+            next_A_idx += 1
         
-        # 되돌아갈때랑 비교해서 판단
-        min_move = min(min_move, i + i + len(name) - next_pos)
-    answer += min_move
-    return answer
+        min_traced = min(min_traced, idx * 2 + len(name) - next_A_idx)
+
+    return min_traced + shifted

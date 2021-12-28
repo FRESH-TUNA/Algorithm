a2c7cc00677dd21a0c7b9cb3b74e90b3cbb07254
lunacircle4 <lunacircle4@gmail.com>
2021-12-28 10:58:51 +0900

    feat: 조이스틱/3

--- 탐욕법/조이스틱.py
+++ 탐욕법/조이스틱.py
@@ -1,15 +1,16 @@
 def solution(name):
-    name = list(name)
-    shifted = 0
-    min_traced = len(name) - 1
+    answer = 0
+    min_lr_shifted = len(name) - 1
     
-    for idx, c in enumerate(name):
-        shifted += min(ord(c) - ord("A"), ord("Z") - ord(c) + 1)    
-        next_A_idx = idx + 1
+    for i, v in enumerate(name):
+        # 상하
+        answer += min(ord(v) - ord("A"), ord("Z") - ord(v) + 1)
         
-        while next_A_idx < len(name) and name[next_A_idx] == "A":
-            next_A_idx += 1
+        # 좌우 최소 계산
+        next_i = i
+        while next_i + 1 < len(name) and name[next_i + 1] == "A":
+            next_i += 1
         
-        min_traced = min(min_traced, idx * 2 + len(name) - next_A_idx)
-
-    return min_traced + shifted
+        min_lr_shifted = min(min_lr_shifted, i + i + len(name) - next_i - 1)
+            
+    return answer + min_lr_shifted

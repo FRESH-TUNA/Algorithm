832e96a207a35f033e3b3c5d4ca5cdefc8bd62a2
lunacircle4 <lunacircle4@gmail.com>
2022-01-01 22:12:23 +0900

    feat: 모음사전

---
+++ 스택/모음사전.py
@@ -0,0 +1,21 @@
+def solution(word):
+    db, stack, answer, word = set(), ["A"], 1, list(word)
+    
+    while stack != word:
+        _word = "".join(stack)
+
+        if _word in db: 
+            pop_or_shift(stack)
+            continue
+        
+        db.add(_word)
+        answer += 1
+        
+        if len(stack) == 5: pop_or_shift(stack)
+        else: stack.append("A")
+    return answer
+
+def pop_or_shift(stack):
+    next_char = {"A":"E", "E":"I", "I":"O", "O":"U"}
+    if stack[-1] == "U": stack.pop()
+    else: stack[-1] = next_char[stack[-1]]

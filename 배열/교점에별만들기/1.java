0b14d9e587cf1fa12db55d4c319c5a64b404b06e
lunacircle4 <lunacircle4@gmail.com>
2022-01-03 11:45:36 +0900

    feat: 교점에 별만들기

--- 배열/교점에별만들기/1.java
+++ 배열/교점에별만들기/1.java
@@ -8,65 +8,59 @@ class Solution {
         for(int i = 0; i < line.length; ++i)
             for(int j = i; j < line.length; ++j)
                 pushIntersect(line, intersects, i, j);
-
+        
         return get_answer(intersects, min_max(intersects));
     }
     
     private void pushIntersect(
         int[][] line, Set<String> intersects, int i, int j
     ) {
-        int[] case_i = line[i], case_j = line[j];
-        
+        int[] case_i = line[i], case_j = line[j]; 
         long r = case_i[0] * case_j[1] - case_j[0] * case_i[1];
+        
         if(r == 0) return;
         
-        double[][] revert = {
-            {(double) case_j[1] / r, (-1.0) * case_i[1] / r}, 
-            {(-1.0) * case_j[0] / r, (double) case_i[0] / r}};
-        double[] intersect = new double[] { 
+        long[][] revert = {
+            {case_j[1], (-1) * case_i[1]}, 
+            {(-1) * case_j[0], case_i[0]}};
+        long[] inter = new long[] { 
             (revert[0][0] * case_i[2] * (-1)
                     + revert[0][1] * case_j[2] * (-1)),
             (revert[1][0] * case_i[2] * (-1) 
                     + revert[1][1] * case_j[2] * (-1))};
-        if(!is_integer(intersect)) return;
         
-        intersects.add((int) intersect[1] + "," + (int) intersect[0]);
+        if(inter[0] % r == 0 && inter[1] % r == 0)
+            intersects.add(inter[0] / r + "," + inter[1] / r);
     }
     
-    private boolean is_integer(double[] intersect) {
-        for(int i = 0; i < intersect.length; ++i)
-            if(intersect[i] - (long)intersect[i] != 0) return false;
-        return true;
-    }
-    
-    private int[] min_max(Set<String> intersects) {
-        int x_min = Integer.MAX_VALUE, y_min = Integer.MAX_VALUE;
-        int x_max = Integer.MIN_VALUE, y_max = Integer.MIN_VALUE;
+    private long[] min_max(Set<String> intersects) {
+        long x_min = Long.MAX_VALUE, y_min = Long.MAX_VALUE;
+        long x_max = Long.MIN_VALUE, y_max = Long.MIN_VALUE;
         for(String intersect : intersects) {
             String[] xy = intersect.split(",");
-            int x = Integer.parseInt(xy[0]);
-            int y = Integer.parseInt(xy[1]);
+            long x = Long.parseLong(xy[0]);
+            long y = Long.parseLong(xy[1]);
             
             x_max = Math.max(x_max, x);
             x_min = Math.min(x_min, x);
             y_max = Math.max(y_max, y);
             y_min = Math.min(y_min, y);
         }
-        return new int[] {x_min, x_max, y_min, y_max};
+        return new long[] {x_min, x_max, y_min, y_max};
     }
     
     private String[] get_answer(
-        Set<String> intersects, int[] min_max
+        Set<String> intersects, long[] min_max
     ) {
-        int MIN_X = min_max[0], MAX_X = min_max[1];
-        int MIN_Y = min_max[2], MAX_Y = min_max[3];
-        String[] answer = new String[Math.abs(MAX_Y - MIN_Y) + 1];
+        long MIN_X = min_max[0], MAX_X = min_max[1];
+        long MIN_Y = min_max[2], MAX_Y = min_max[3];
+        String[] answer = new String[(int) Math.abs(MAX_Y - MIN_Y) + 1];
         
-        for(int i = 0, _i = MAX_Y; _i >= MIN_Y; ++i, --_i) {
+        for(long i = 0, _i = MAX_Y; _i >= MIN_Y; ++i, --_i) {
             StringBuilder sb = new StringBuilder();
-            for(int _j = MIN_X; _j <= MAX_X; ++_j) 
-                sb.append(intersects.contains(_i + "," + _j) ? "*" : ".");
-            answer[i] = sb.toString();
+            for(long _j = MIN_X; _j <= MAX_X; ++_j) 
+                sb.append(intersects.contains(_j + "," + _i) ? "*" : ".");
+            answer[(int) i] = sb.toString();
         }
 
         return answer;

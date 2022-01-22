8130a776e725dd4036a7f65e7bd57c6bed5fcba5
DONGWON KIM <lunacircle4@likelion.org>
2022-01-22 18:36:55 +0900

    feat: 조합

--- DFS/순열 (leetcode).py
+++ DFS/순열 (leetcode).py
@@ -12,3 +12,19 @@ class Solution:
                     if num in db: continue
                     stack.append(({x for x in db} | {num}, _ans + [num]))
         return answer
+
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        self.dfs(nums, ans, [])
+        return ans
+    
+    def dfs(self, nums, ans, cur):
+        if len(cur) == len(nums): ans.append(cur)
+        else:
+            for num in nums:
+                if num not in cur: 
+                    self.dfs(nums, ans, cur + [num])
+            

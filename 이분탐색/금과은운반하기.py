d7d15f7d2e6e189b592ea1a2b2e9fb1e279a2aeb
DONGWON KIM <lunacircle4@likelion.org>
2022-01-26 20:40:36 +0900

    feat: 금과은 운반하기

---
+++ 이분탐색/금과은운반하기.py
@@ -0,0 +1,31 @@
+def solution(a, b, g, s, w, t):
+    start, end = 0, (10**9) * 2 * (10**5) * 2    
+    cities = zip(g, s, w, t)
+    LEN_CITIES, answer = len(t), end
+
+    while start <= end:
+        mid = (start + end) // 2
+        gold, silver, total = 0, 0, 0
+        
+        for city in cities:
+            _gold, _silver, _weight, _time = city
+            
+            # 시간동안 도시에서 자원을 가지고 올수 있는 횟수
+            moves = mid // (_time * 2)
+            if mid % (_time * 2) >= _time: moves += 1
+            
+            # 최대 골드/실버/(골드, 실버)
+            gold += _gold if (_gold < moves * _weight
+                             ) else (moves * _weight)
+            silver += _silver if (_silver < moves * _weight
+                             ) else (moves * _weight)
+            total += _gold + _silver if(
+                _gold + _silver < moves * _weight
+                ) else moves * _weight
+            
+            # 총용량이 크고 , 골드 실버가 모두 커야만 답이 될수 있다.
+            if gold >= a and silver >= b and total >= a+b:
+                end, answer = mid - 1, min(answer, mid)
+            else: start = mid + 1
+
+    return answer

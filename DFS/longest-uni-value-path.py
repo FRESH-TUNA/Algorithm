c2907fc04f2405e3e6b4830315be8966db4b8557
DONGWON KIM <lunacircle4@likelion.org>
2022-01-28 17:11:39 +0900

    feat: longest uni value path

--- DFS/longest-uni-value-path.py
+++ DFS/longest-uni-value-path.py
@@ -8,22 +8,18 @@ class Solution:
     ans = 0
     def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
         def dfs(node):
-            if not node:
-                return 0
+            if not node: return 0
 
-            left = dfs(node.left)
-            right = dfs(node.right)
-            
-            if node.left and node.left.val == node.val;
-                left += 1
-            else: left = 0
-                
-            if node.right and node.right.val == node.val;
-                right += 1
-            else: right = 0
-            
-            self.result = max(self.result, left + right)
-            return max(left, right)
+            _ans = [0]
+            l_ans, r_ans = dfs(node.left), dfs(node.right)
+
+            if node.left and node.left.val == node.val:
+                _ans.append(l_ans+1)
+            if node.right and node.right.val == node.val:
+                _ans.append(r_ans+1)
+
+            self.ans = max(sum(_ans), self.ans)
+            return max(_ans)
         
         dfs(root)
         return self.ans

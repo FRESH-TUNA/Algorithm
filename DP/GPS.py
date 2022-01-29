3728a052c9ad288dd477c25df111d2fd8d3c1bd8
DONGWON KIM <lunacircle4@likelion.org>
2022-01-30 00:43:58 +0900

    feat: GPS

--- DP/GPS.py
+++ DP/GPS.py
@@ -6,12 +6,13 @@ class Solution {
     private final int MAX_NODES = 200;
     private final int MAX_TRY = 100;
     private Set<Integer>[] graph = new Set[MAX_NODES+1];
-    private int[][] ans = new int[MAX_TRY+1][MAX_NODES+1];
+    private int[][] ans = new int[MAX_TRY][MAX_NODES+1];
     
     public int solution(int n, int m, int[][] edge_list, int k, int[] gps_log) {
         init_graph(n, edge_list);
         adjustment(k, n, gps_log);
-        return ans[k][n] >= k ? -1 : ans[k][n];
+        int final_ans = ans[k-1][gps_log[k-1]];
+        return final_ans >= k ? -1 : final_ans;
     }
     
     private void init_graph(int n, int[][] edge_list) {
@@ -27,16 +28,16 @@ class Solution {
     }
     
     private void adjustment(int k, int n, int[] gps_log) {
-        for(int j = 1; j <= n; ++j) ans[1][j] = k;
-        ans[1][gps_log[0]] = 0;
+        for(int j = 1; j <= n; ++j) ans[0][j] = k;
+        ans[0][gps_log[0]] = 0;
         
-        for(int i = 2; i <= k; ++i) {
+        for(int i = 1; i < k; ++i) {
             int[] prev = ans[i-1];
             for(int j = 1; j <= n; ++j) {
-                int _ans = graph[j].stream()
+                ans[i][j] = graph[j].stream()
                     .map(x -> prev[x])
                     .min(Integer::compare).get();
-                ans[i][j] = gps_log[i-1] == j ? _ans : _ans+1;
+                ans[i][j] += gps_log[i] == j ? 0 : 1;
             }
         }       
     }

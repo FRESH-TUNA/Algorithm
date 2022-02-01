aff2c04be85bc2f0334f95d187dbc4952da8d381
DONGWON KIM <lunacircle4@likelion.org>
2022-02-01 14:26:11 +0900

    feat: construct binary tree

---
+++ 트리/construct-binary-tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int], idx=0) -> Optional[TreeNode]:
+        if inorder:
+            index = inorder.index(preorder.pop(0))
+
+            node = TreeNode(inorder[index])
+            node.left = self.buildTree(preorder, inorder[0:index])
+            node.right = self.buildTree(preorder, inorder[index+1:])
+
+            return node
--- 트리/minimum-distance-between-bst-nodes.py
+++ 트리/minimum-distance-between-bst-nodes.py
@@ -17,4 +17,44 @@ class Solution:
         
         return min(y-x for x, y in zip(
             nodes[:-1], nodes[1:]))
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    prev = -sys.maxsize
+    result = sys.maxsize
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if root.left: dfs(root.left)
+            self.result = min(
+                self.result, root.val - self.prev)
+            self.prev = root.val
+            if root.right: dfs(root.right)
+        
+        dfs(root)
+        return self.result
+            
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    prev = -sys.maxsize
+    result = sys.maxsize
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if root.left: dfs(root.left)
+            self.result = min(
+                self.result, root.val - self.prev)
+            self.prev = root.val
+            if root.right: dfs(root.right)
+        
+        dfs(root)
+        return self.result
             

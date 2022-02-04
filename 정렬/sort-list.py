a52e1fe224142b8f5c750e1f88592248e7576839
DONGWON KIM <lunacircle4@likelion.org>
2022-02-04 18:04:19 +0900

    feat: sort-list

---
+++ 정렬/sort-list.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # slow == mid
+        if not (head and head.next): return head
+        half, slow, fast = None, head, head
+        while fast and fast.next:
+            half, slow, fast = slow, slow.next, fast.next.next
+            
+        # 리스트를 끊어준다.
+        half.next = None
+        
+        l1 = self.sortList(head)
+        l2 = self.sortList(slow)
+        return self.mergeTwoLists(l1, l2)
+          
+    def mergeTwoLists(self, l1, l2):
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1 or l2

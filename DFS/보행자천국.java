c7f59528a508772accd72d4d2293c7478dce15c1
DONGWON KIM <lunacircle4@likelion.org>
2022-02-11 00:45:39 +0900

    feat: 보행자천국

--- DFS/보행자천국.java
+++ DFS/보행자천국.java
@@ -6,6 +6,7 @@ class Solution {
         List<int[]> stack = new LinkedList();
         int[] node = new int[] {0, 0, -1};
         int MOD = 20170805;
+        int answer
         boolean[][] traced = new boolean[m][n]; 
         
         stack.append(node);
@@ -13,20 +14,28 @@ class Solution {
         
         while(!stack.isEmpty()) {
             while(true) {
-                int[] case = stack.getLast();
-                int r=case[0], c=case[1], d=case[2];
-                int[][] new_cases = new int[4][3] {
+                int[] node = stack.getLast();
+                int r=node[0], c=node[1], d=node[2];
+                int[][] new_nodes = new int[4][3] {
                     {r+1,c, 0}, {r-1,c,1}, {r,c+1,2}, {r,c-1,3}};
                 
-                for(int[] case : new_cases) {
-                    int nr=case[0], nc=case[1], nd=case[2];
+                for(int[] new_node : new_nodes) {
+                    int nr=new_node[0], nc=new_node[1], nd=new_node[2];
                     
-                    if(nr == -1 || nr == m) continue
-                    if(nc == -1 || nc == n) continue
-                    if(traced[nr][nc] || graph[nr][nc] == 1) continue
-                    if(graph[nr][nc] == 2 and )
+                    if(nr == -1 || nr == m) continue;
+                    if(nc == -1 || nc == n) continue;
+                    if(traced[nr][nc] || graph[nr][nc] == 1) continue;
+                    if(graph[nr][nc] == 2 && d != nd && r != 0 && c != 0)
+                        continue;
                     traced[nr][nc] = true;
-                     
+                    stack.push(new int{nr, nc, nd});
+                    break;
                 }
+                if(case == stack.getLast()) break;
             }
-        }s
+        }
+        
+        int answer = 0;
+        return answer;
+    }
+}

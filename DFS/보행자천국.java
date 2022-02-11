c0e6c2be2c49fdb5d877053874322bed059761c6
DONGWON KIM <lunacircle4@likelion.org>
2022-02-11 12:03:35 +0900

    feat: 보행자 천국

--- DFS/보행자천국.java
+++ DFS/보행자천국.java
@@ -1,22 +1,20 @@
 import java.util.LinkedList;
-import java.util.List;
 
 class Solution {
     public int solution(int m, int n, int[][] cityMap) {
-        List<int[]> stack = new LinkedList();
-        int[] node = new int[] {0, 0, -1};
+        LinkedList<int[]> stack = new LinkedList();
         int MOD = 20170805;
-        int answer
+        int answer = 0;
         boolean[][] traced = new boolean[m][n]; 
         
-        stack.append(node);
+        stack.push(new int[] {0, 0, -1});
         traced[0][0] = true;
         
         while(!stack.isEmpty()) {
             while(true) {
                 int[] node = stack.getLast();
                 int r=node[0], c=node[1], d=node[2];
-                int[][] new_nodes = new int[4][3] {
+                int[][] new_nodes = new int[][] {
                     {r+1,c, 0}, {r-1,c,1}, {r,c+1,2}, {r,c-1,3}};
                 
                 for(int[] new_node : new_nodes) {
@@ -24,18 +22,22 @@ class Solution {
                     
                     if(nr == -1 || nr == m) continue;
                     if(nc == -1 || nc == n) continue;
-                    if(traced[nr][nc] || graph[nr][nc] == 1) continue;
-                    if(graph[nr][nc] == 2 && d != nd && r != 0 && c != 0)
+                    if(traced[nr][nc] || cityMap[nr][nc] == 1) continue;
+                    if(cityMap[nr][nc] == 2 && d != nd && r != 0 && c != 0)
                         continue;
                     traced[nr][nc] = true;
-                    stack.push(new int{nr, nc, nd});
+                    if(nr == m-1 && nc == n-1) answer += 1;
+                    stack.push(new int[]{nr, nc, nd});
                     break;
                 }
-                if(case == stack.getLast()) break;
+                if(node == stack.getLast()) break;
             }
+            
+            int[] popped = stack.pop();
+            int pr=popped[0], pc=popped[1];
+            traced[pr][pc] = false;
         }
-        
-        int answer = 0;
+
         return answer;
     }
 }

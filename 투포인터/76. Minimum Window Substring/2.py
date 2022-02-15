3bb0b3f069c91e7e41ca6856dc7720fe3870cfbe
DONGWON KIM <lunacircle4@likelion.org>
2022-02-15 20:09:48 +0900

    feat: minimum window substring

--- 투포인터/76. Minimum Window Substring/2.py
+++ 투포인터/76. Minimum Window Substring/2.py
@@ -1,23 +1,23 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         BORDER = 100001
-        db = collections.Counter(t)
-        target_size = len(t)
         start, end = 0, BORDER
         left = 0
-        
-        for right, value in enumerate(s):
-            target_size -= db[value] > 0
-            db[value] -= 1
+        db = collections.Counter(t)
+        db_size = len(t)
 
-            if target_size == 0:
-                while left < right+1 and db[s[left]] < 0:
+        for right, value in enumerate(s, 1):
+            db_size -= db[value] > 0
+            db[value] -= 1
+            
+            if not db_size:
+                while left < right and db[s[left]] < 0:
                     db[s[left]] += 1
                     left += 1
-                if right-left+1 < end-start:
-                    start, end = left, right+1
+                if right - left <= end - start:
+                    start, end = left, right
                     db[s[left]] += 1
                     left += 1
-                    target_size += 1
+                    db_size += 1
 
         return "" if end == BORDER else s[start:end]

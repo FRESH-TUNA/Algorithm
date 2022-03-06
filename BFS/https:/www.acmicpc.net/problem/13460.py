bab36b4c583be03a4c11d819cbaf0e30bc58f58d
DONGWON KIM <lunacircle4@likelion.org>
2022-03-06 23:25:18 +0900

    feat: 13460

--- BFS/https:/www.acmicpc.net/problem/13460.py
+++ BFS/https:/www.acmicpc.net/problem/13460.py
@@ -1,16 +1,16 @@
 from collections import deque
 import sys
 
-        
 def bfs(rx, ry, bx, by, graph):
     Q = deque()
-    visited = {}
+    visited = set()
     count = 0
     dx, dy = [-1, 1, 0, 0], [0, 0, -1 ,1]
 
+    Q.append((rx, ry, bx, by))
     while Q:
-        for _ in range(len(q)):
-            rx, ry, bx, by = q.popleft()
+        for _ in range(len(Q)):
+            rx, ry, bx, by = Q.popleft()
             if count > 10: return -1
             if graph[rx][ry] == 'O': return count
             for d in range(4):
@@ -26,13 +26,13 @@ def bfs(rx, ry, bx, by, graph):
                         break
                 nbx, nby = bx, by
                 while True:
-                    nrx += dx[d]
-                    nry += dy[d]
-                    if graph[nrx][nry] == '#':
-                        nrx -= dx[d]
-                        nry -= dy[d]
+                    nbx += dx[d]
+                    nby += dy[d]
+                    if graph[nbx][nby] == '#':
+                        nbx -= dx[d]
+                        nby -= dy[d]
                         break
-                    if graph[nrx][nry] == 'O':
+                    if graph[nbx][nby] == 'O':
                         break
                 if graph[nbx][nby] == 'O': continue
 
@@ -42,25 +42,25 @@ def bfs(rx, ry, bx, by, graph):
                         nry -= dy[d]
                     else:
                         nbx -= dx[d]
-                        nby -= dx[d]
-                
+                        nby -= dy[d]
+            
                 if (nrx, nry, nbx, nby) not in visited:
                     Q.append((nrx, nry, nbx, nby))
                     visited.add((nrx, nry, nbx, nby))
         count += 1
-        return -1
+    return -1
             
-
+ 
 # driver
 input = sys.stdin.readline
 n, m = map(int, input().split())
 graph = []
 for i in range(n):
-    graph.append(list(input))
+    graph.append(list(input()))
     for j in range(m):
         if graph[i][j] == 'R':
             rx, ry = i, j
         elif graph[i][j] == 'B':
             bx, by = i, j
 
-print(bfs(n, m, graph))
+print(bfs(rx, ry, bx, by, graph))

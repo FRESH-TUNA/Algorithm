eb7da22090683afa796924fdb1b97221b0a601f3
DONGWON KIM <lunacircle4@likelion.org>
2022-03-06 14:44:42 +0900

    feat: ㅈ너ㄱ사

--- DP/상자넣기.py
+++ DP/상자넣기.py
@@ -1,1 +1,17 @@
-def
+from sys import stdin
+def solution(N, GRAPH):
+    ans, ans[0][0] = [[0] * N for _ in range(N)], 1
+    
+    for i in range(N):
+        for j in range(N):
+            jump = GRAPH[i][j]
+            if jump and ans[i][j]:
+                if i + jump < N: ans[i + jump][j] += ans[i][j]
+                if j + jump < N: ans[i][j + jump] += ans[i][j]
+    
+    return ans[-1][-1]
+
+# driver
+N = int(stdin.readline())
+GRAPH = [list(map(int, stdin.readline().split())) for _ in range(N)]
+print(solution(N, GRAPH))

e6d385f680227e1cefcaa57ff17a6332c5b30faa
DONGWON KIM <lunacircle4@likelion.org>
2022-03-13 00:51:35 +0900

    feat: 15684

--- 시뮬레이션/https:/www.acmicpc.net/problem/15684.py
+++ 시뮬레이션/https:/www.acmicpc.net/problem/15684.py
@@ -3,36 +3,49 @@ import sys
 # global
 res = sys.maxsize
 
-#https://suri78.tistory.com/212
+def solution(N, H, ladder):
+    dfs(N, H, ladder, 1, 1, 0)
+    return res if res < 4 else -1
 
-def solution(N, M, H, ladder):
-    for d in range(4):
-        dfs(N, ladder, 0, 0, r)
-        if res != sys.maxsize: return res
-    return -1
-
-def dfs(N, ladder, cnt, i, r):
-    if cnt == r:
-        if move(): res = cnt
+def dfs(N, H, ladder, i, j, cnt):
+    global res
+    if check(N, H, ladder): 
+        res = min(res, cnt)
         return
+    # 브릿지가 3개거나 최적값이 아니면 바로 return
+    elif cnt == 3 or res <= cnt: return
 
-    for i in range(idx, h):
-        for j in range(N):
-            if ladder[j][i]: continue
-            if j-1 >= 0 and ladder[j-1][i]: continue
-            if j+1 < N and ladder[j+1][i]
-            ladder[j][i] = 1
-            dfs(cnt+1, i, r)
-            ladder[j][i] = 0
+    # 가로선
+    for x in range(i, H+1):
+        # 가로선이 같으면 주어진 세로선 부터 탐색
+        k = j if x == i else 1
+        # 세로선
+        for y in range(k, N):
+            if not ladder[x][y] and not ladder[x][y+1]:
+                ladder[x][y] = 1
+                # 세로선을 이어서 붙이지 못해서 +2 한다
+                dfs(N, H, ladder, i, j+2, cnt+1)
+                ladder[x][y] = 0
+        
+
+# 모든 세로선에 대해 자신의 세로선으로 돌아가는지 check 한다
+def check(N, H, ladder):
+    for start in range(1, N+1):
+        i = start
+        for h in range(1, H+1):
+            if ladder[h][i]: i += 1
+            elif i-1 and ladder[h][i-1]: i -= 1
+        if i != start: return False
+    return True
 
 # driver
 input = sys.stdin.readline
-N, M, H = map(int, input.split())
-
-# 각 세로선당 가로선
-ladder = [[0]*(H+1) for _ in range(N+1)]
+N, M, H = map(int, input().split())
+sys.setrecursionlimit(10 ** 6)
+# 각 가로선당 세로선
+ladder = [[0]*(N+1) for _ in range(H+1)]
 for _ in range(M):
     # 가로선 / 세로선
     x, y = map(int, input().split())
-    ladder[y][x] = 1
-print(solution(N, M, H, ladder))
+    ladder[x][y] = 1
+print(solution(N, H, ladder))

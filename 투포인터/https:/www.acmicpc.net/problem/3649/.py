eca89ddc14708dcfc7b0c8ad9942d86ddc0a3993
DONGWON KIM <lunacircle4@likelion.org>
2022-05-01 00:11:32 +0900

    feat: 3649

--- 투포인터/https:/www.acmicpc.net/problem/3649/.py
+++ 투포인터/https:/www.acmicpc.net/problem/3649/.py
@@ -6,15 +6,16 @@ while True:
         blank, N = int(input()) * 10000000, int(input())
         blocks = sorted(int(input()) for _ in range(N))
         left, right = 0, N-1
+        flag = False
         
         while left < right:
             res = blocks[left] + blocks[right]
-
             if res == blank:
-                print("yes " + blocks[left] + " " + blocks[right])
+                flag = True
+                print("yes " + str(blocks[left]) + " " + str(blocks[right]))
                 break
             elif res > blank: right -= 1
             else: left += 1
+        if not flag: print("danger")
     except:
         break
-    

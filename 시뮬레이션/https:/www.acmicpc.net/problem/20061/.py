c08afb0ecf25eca3b1f033df3314f6159a93df1b
DONGWON KIM <lunacircle4@likelion.org>
2022-05-05 01:00:09 +0900

    feat: 20061

--- 시뮬레이션/https:/www.acmicpc.net/problem/20061/.py
+++ 시뮬레이션/https:/www.acmicpc.net/problem/20061/.py
@@ -4,7 +4,8 @@ import sys, math
 input = sys.stdin.readline
 BORDER = 10
 G = [[0]*BORDER for _ in range(BORDER)]
-
+N = int(input())
+DATAS = [list(map(int, input().rstrip().split())) for _ in range(N)]
 
 def solution():
     edges = sorted([(datas[i], datas[i+1]) for i in range(len(datas)-1)], 
@@ -17,10 +18,34 @@ def solution():
         res -= (y-x)
     print(res)
 
+def blue_remove():
+    b, p = N-1, N-1
 
-def blue_postprocessing():
+    while p >= 0:
+        if G[0][p] and G[1][p] and G[2][p] and G[3][p]:
+            p -= 1
+        else:
+            G[0][b], G[1][b] = G[0][p], G[1][p]
+            G[2][b], G[3][b] = G[2][p], G[3][p]
+            b -= 1
+
+    for i in range(b+1):
+        G[0][i] = G[1][i] = G[2][i] = G[3][i] = 0
     
+def blue_shift():
+    n = 0
+    if G[0][4] or G[1][4] or G[2][4] or G[3][4]: n += 1
+    if G[0][5] or G[1][5] or G[2][5] or G[3][5]: n += 1
 
+    for i in range(N-1-n, -1, -1):
+        G[0][i+n], G[1][i+n] = G[0][i], G[1][i]
+        G[2][i+n], G[3][i+n] = G[2][i], G[3][i]
+
+    for i in range(n):
+        G[0][i] = G[1][i] = G[2][i] = G[3][i] = 0
+        
+
+    
 def blue_border_check(blocks):
     for x, y in blocks:
         if y == BORDER-1 or G[x][y+1]: return True

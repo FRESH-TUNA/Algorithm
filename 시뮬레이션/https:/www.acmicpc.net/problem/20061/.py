3d2291e23dc5d2f9952e006066a363d551018b18
DONGWON KIM <lunacircle4@likelion.org>
2022-05-05 01:06:41 +0900

    feat: 20061

--- 시뮬레이션/https:/www.acmicpc.net/problem/20061/.py
+++ 시뮬레이션/https:/www.acmicpc.net/problem/20061/.py
@@ -6,24 +6,29 @@ BORDER = 10
 G = [[0]*BORDER for _ in range(BORDER)]
 N = int(input())
 DATAS = [list(map(int, input().rstrip().split())) for _ in range(N)]
+RES = 0
 
 def solution():
-    edges = sorted([(datas[i], datas[i+1]) for i in range(len(datas)-1)], 
-                key=lambda x: x[1]-x[0], reverse=True)
-    res, count = datas[-1] - datas[0], 1
+    for t, x, y in DATAS:
+        if t == 1: gaming([[x, y]])
+        elif t == 2: gaming([[x, y], [x, y+1]])
+        else: gaming([[x, y], [x+1, y]])
+    print(RES)
 
-    for x, y in edges:
-        if count >= T: break
-        count += 1
-        res -= (y-x)
-    print(res)
+def gaming(blocks):
+    blue_move(blocks)
+    blue_remove()
+    blue_shift()
+
 
 def blue_remove():
+    global RES
     b, p = N-1, N-1
 
-    while p >= 0:
+    while p >= 0 and b >= 0:
         if G[0][p] and G[1][p] and G[2][p] and G[3][p]:
             p -= 1
+            RES += 1
         else:
             G[0][b], G[1][b] = G[0][p], G[1][p]
             G[2][b], G[3][b] = G[2][p], G[3][p]
@@ -33,17 +38,18 @@ def blue_remove():
         G[0][i] = G[1][i] = G[2][i] = G[3][i] = 0
     
 def blue_shift():
+    global RES
     n = 0
     if G[0][4] or G[1][4] or G[2][4] or G[3][4]: n += 1
     if G[0][5] or G[1][5] or G[2][5] or G[3][5]: n += 1
 
+    RES += n
     for i in range(N-1-n, -1, -1):
         G[0][i+n], G[1][i+n] = G[0][i], G[1][i]
         G[2][i+n], G[3][i+n] = G[2][i], G[3][i]
 
     for i in range(n):
         G[0][i] = G[1][i] = G[2][i] = G[3][i] = 0
-        
 
     
 def blue_border_check(blocks):

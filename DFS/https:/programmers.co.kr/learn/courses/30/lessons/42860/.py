ba95daa4662a93a3fab31f66689d0d61e184bad9
DONGWON KIM <lunacircle4@likelion.org>
2022-05-14 00:46:17 +0900

    feat: 42860 너무짜증난다

--- DFS/https:/programmers.co.kr/learn/courses/30/lessons/42860/.py
+++ DFS/https:/programmers.co.kr/learn/courses/30/lessons/42860/.py
@@ -1,37 +1,39 @@
 answer = 1000000000
-N, finish, traced = 0, 0, []
+N, finish, traced = 0, 0, set()
 
 def solution(name):
-    global N, finish, traced
+    global N, finish
 
     N = len(name)
     updown_answer = 0
-    traced = [0] * len(name)
     
     # up_down
     for idx, char in enumerate(name):
         updown_answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
         if char != 'A': finish += 1
+    if finish == 0: return 0
+    if name[0] == 'A': finish += 1
 
     # dfs
-    traced[0] = 1
-    dfs(name, 0, 0, 1, updown_answer)
+    traced.add(0)
+    dfs(name, 0, updown_answer)
     return answer
 
-def dfs(name, last_pos, cur_pos, depth, cur):
+def dfs(name, cur_pos, cur):
     global answer
-    if depth == finish:
+
+    if len(traced) == finish:
         answer = min(answer, cur)
         return
 
     for n in range(N):
-        if traced[n] or name[n] == 'A': continue
-            
+        if n in traced or name[n] == 'A': continue
+        
         n_cur = cur
         if cur_pos > n: n_cur += min(cur_pos-n, N-cur_pos+n)
         else: n_cur += min(n-cur_pos, cur_pos+N-n)
         if n_cur >= answer: continue
             
-        traced[n] = 1
-        dfs(name, cur_pos, n, depth+1, n_cur)
-        traced[n] = 0
+        traced.add(n)
+        dfs(name, n, n_cur)
+        traced.remove(n)

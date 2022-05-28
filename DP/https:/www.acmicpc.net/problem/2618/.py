7e733f3cbb8664dffb4b696e56ad15c64863e59d
DONGWON KIM <lunacircle4@likelion.org>
2022-05-28 17:50:41 +0900

    feat: 2616

--- DP/https:/www.acmicpc.net/problem/2618/.py
+++ DP/https:/www.acmicpc.net/problem/2618/.py
@@ -1,39 +1,37 @@
 import sys
-sys.setrecursionlimit(10 ** 5)
-
-# global
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+# main
 N, W = int(input()), int(input())
-Dests = [list(map(int, input().split())) for _ in range(W)]
-DB = [[-1 for _ in range(W+1)] for _ in range(W+1)]
-PolicesDB = []
+events = [(1,1),(N,N)]
+for _ in range(W):
+    events.append(tuple(map(int,input().split())))
+DB = [[-1 for _ in range(W+2)] for _ in range(W+2)]
 
-def distance(police, target, start):
-    police_x, police_y, target_x, target_y = 0, 0, 0, 0
-    target_x, target_y = Dests[target-1]
+def solution(car1, car2):
+    if car1 == W+1 or car2 == W+1: return 0
+    if DB[car1][car2] != -1: return DB[car1][car2]
     
-    if start == 1:
-        police_x, police_y = 1, 1
-    elif start == 2:
-        police_x, police_y = N, N
-    else:
-        police_x, police_y = Dests[police-1]
- 
-    return abs(police_x-target_x) + abs(police_y-target_y)
-
-def dfs(car1, car2):
-    if car1 == W or car2 == W:
-        return 0
-    if DB[car1][car2] != -1:
-        return DB[car1][car2]
-    target = max(car1, car2)
-
-    car1_res = dfs(target, car2) + distance(car1, target, [0, 1][car1==0])
-    car2_res = dfs(car1, target) + distance(car2, target, [0, 2][car2==0])
-
-    DB[car1][car2] = min(tmp1,tmp2);
-    PolicesDB[car1][car2] = [2, 1][car1_res < car2_res]
+    target = max(car1, car2) + 1
+    car1_v = solution(target, car2) + abs(events[target][0]-events[car1][0]) + abs(events[target][1]-events[car1][1])
+    car2_v = solution(car1, target) + abs(events[target][0]-events[car2][0]) + abs(events[target][1]-events[car2][1])
+    DB[car1][car2] = min(car1_v, car2_v)
     return DB[car1][car2]
 
-# drivers
-print(dfs(0, 0))
+def trace(car1, car2):
+    if car1 == W+1 or car2 == W+1: return
+    target = max(car1, car2) + 1
+    car1_v = abs(events[target][0]-events[car1][0]) + abs(events[target][1]-events[car1][1])
+    car2_v = abs(events[target][0]-events[car2][0]) + abs(events[target][1]-events[car2][1])
+
+    if DB[target][car2]+car1_v < DB[car1][target]+car2_v:
+        print(1)
+        trace(target, car2)
+    else:
+        print(2)
+        trace(car1, target)
+
+# result
+print(solution(0, 1))
+trace(0, 1)

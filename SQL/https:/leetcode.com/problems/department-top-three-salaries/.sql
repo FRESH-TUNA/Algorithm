5fae087fbc9fa16ac1cc4de9c8116f4c330cede2
DONGWON KIM <lunacircle4@likelion.org>
2022-06-04 16:19:57 +0900

    feat: top three

--- SQL/https:/leetcode.com/problems/department-top-three-salaries/.sql
+++ SQL/https:/leetcode.com/problems/department-top-three-salaries/.sql
@@ -1,13 +1,6 @@
-# Write your MySQL query statement below
-
-
-
-GROUP BY departmentId
-
-
-
-
-
-
-
-GROUP BY salary
+SELECT D.Name as Department, E.Name as Employee, E.Salary 
+FROM Department D, Employee E, Employee E2
+WHERE D.ID = E.DepartmentId and E.DepartmentId = E2.DepartmentId and 
+E.Salary <= E2.Salary
+group by D.ID,E.Name having count(distinct E2.Salary) <= 3
+order by D.Name, E.Salary desc

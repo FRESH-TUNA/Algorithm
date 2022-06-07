2218dffac7db30308ba8b58d5e7882877e8f65f6
DONGWON KIM <lunacircle4@likelion.org>
2022-06-08 00:14:16 +0900

    feat: 2nd array knapsack - modify2

--- DP/2nd-array-unbounded-knapsack/.py
+++ DP/2nd-array-unbounded-knapsack/.py
@@ -1,4 +1,5 @@
 def solution(sugar, salt, stuffs):
+    print(stuffs)
     MAX_SUGAR = max(s[0] for s in stuffs) 
     MAX_SALT = max(s[1] for s in stuffs)
     MAX_TIME = MAX_SUGAR + MAX_SALT
@@ -10,27 +11,25 @@ def solution(sugar, salt, stuffs):
     for su in range(sugar+1):
         for sa in range(salt+1):
             DB[su][sa] = 0
-
-    for _ in range(N):
-        for rSugar, rSalt, dSugar, dSalt, time in stuffs:
-            for su in range(MAX_SUGAR+1):
-                for sa in range(MAX_SALT+1):
-                    prevSu, prevSa = su-dSugar, sa-dSalt
-                    if prevSu < rSugar or prevSa < rSalt:
-                        continue
-                    else:
-                        DB[su][sa] = min(
-                            DB[su][sa],
-                            DB[prevSu][prevSa]+time,
-                        )
+    
+    for su in range(MAX_SUGAR+1):
+        for sa in range(MAX_SALT+1):
+            for rSugar, rSalt, dSugar, dSalt, time in stuffs:
+                prevSu, prevSa = su-dSugar, sa-dSalt
+                if prevSu < rSugar or prevSa < rSalt:
+                    continue
+                else:
+                    DB[su][sa] = min(
+                        DB[su][sa],
+                        DB[prevSu][prevSa]+time,
+                    )
     return DB[-1][-1]
 
 # driver
-a, c = 0, 0
-s = [[0,0,2,1,2],[4,5,3,1,2],[4,11,4,0,2],[10,4,0,4,2]]
+a, c = 10, 10
+s = [[10,15,2,1,2],[20,20,3,3,4]]
 
 from itertools import permutations
 
 for _s in permutations(s):
     print(solution(a, c, list(_s)))
-

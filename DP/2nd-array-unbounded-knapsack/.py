cf9dee7f81d1962e04ff7a46c86065778e0cd8a4
DONGWON KIM <lunacircle4@likelion.org>
2022-06-08 00:07:05 +0900

    feat: 2nd array knapsack - modify

--- DP/2nd-array-unbounded-knapsack/.py
+++ DP/2nd-array-unbounded-knapsack/.py
@@ -1,37 +1,36 @@
 def solution(sugar, salt, stuffs):
-    print(stuffs)
     MAX_SUGAR = max(s[0] for s in stuffs) 
     MAX_SALT = max(s[1] for s in stuffs)
     MAX_TIME = MAX_SUGAR + MAX_SALT
-    stuffs = stuffs + [[0,0,1,0,1], [0,0,0,1,1]]
+    stuffs = [[0,0,1,0,1], [0,0,0,1,1]] + stuffs
     N = len(stuffs)
-    DB = [[[MAX_TIME for _ in range(MAX_SALT+1)] 
-        for _ in range(MAX_SUGAR+1)] for _ in range(N+1)]
+    DB = [[MAX_TIME for _ in range(MAX_SALT+1)] for _ in range(MAX_SUGAR+1)]
 
     # init
     for su in range(sugar+1):
         for sa in range(salt+1):
-            DB[0][su][sa] = 0
+            DB[su][sa] = 0
 
-    for n in range(1, N+1):
-        rSugar, rSalt, dSugar, dSalt, time = stuffs[n-1]
-        for su in range(MAX_SUGAR+1):
-            for sa in range(MAX_SALT+1):
-                prevSu, prevSa = su-dSugar, sa-dSalt
-                if prevSu < rSugar or prevSa < rSalt:
-                    DB[n][su][sa] = DB[n-1][su][sa]
-                DB[n][su][sa] = min(
-                    DB[n][su][sa], 
-                    DB[n][prevSu][prevSa]+time,
-                    DB[n-1][prevSu][prevSa]+time
-                )
-    return DB[-1][-1][-1]
+    for _ in range(N):
+        for rSugar, rSalt, dSugar, dSalt, time in stuffs:
+            for su in range(MAX_SUGAR+1):
+                for sa in range(MAX_SALT+1):
+                    prevSu, prevSa = su-dSugar, sa-dSalt
+                    if prevSu < rSugar or prevSa < rSalt:
+                        continue
+                    else:
+                        DB[su][sa] = min(
+                            DB[su][sa],
+                            DB[prevSu][prevSa]+time,
+                        )
+    return DB[-1][-1]
 
 # driver
-a, c = 10, 10
-s = [[10,15,2,1,2],[20,20,3,3,4]]
+a, c = 0, 0
+s = [[0,0,2,1,2],[4,5,3,1,2],[4,11,4,0,2],[10,4,0,4,2]]
 
 from itertools import permutations
 
 for _s in permutations(s):
     print(solution(a, c, list(_s)))
+

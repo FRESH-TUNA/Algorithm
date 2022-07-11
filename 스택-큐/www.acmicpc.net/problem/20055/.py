d294f86ee760b41c6c465b754e6d09a8dad34450
dongwon kim <freshtuna@kakao.com>
2022-07-11 13:07:12 +0900

    백준 20055, 컨베이어 벨트 위의 로봇, 2번째 풀이

--- 스택-큐/www.acmicpc.net/problem/20055/.py
+++ 스택-큐/www.acmicpc.net/problem/20055/.py
@@ -4,33 +4,28 @@ from collections import deque
 def solution():
     input = sys.stdin.readline
     N, K = map(int, input().split())
-    C, R = deque(list(map(int, input().split()))), deque([0]*N)
-    answer, k = 0, 0
+    BELT, ROBOT = deque(list(map(int, input().split()))), deque([0]*(N))
+    EMPTY, GET = [0], 0
 
-    while True:
-        C.rotate(1)
-        R.rotate(1)
-        R[-1] = 0
+    for answer in range(1, N*max(BELT)*2+1):
+        BELT.rotate(1)
+        ROBOT.rotate(1)
+        ROBOT[-1] = 0
         
-        if sum(R):
-            # 가장 마지막위치가 가장 먼저 로봇이 들어간 위치가 될수 있다.
-            for i in range(N-2, -1, -1):
-                if R[i] == 1 and R[i+1] == 0 and C[i+1] >= 1:
-                    R[i+1], R[i] = 1, 0
-                    C[i+1] -= 1
-                    k += [0,1][C[i+1] == 0]
-            # 마지막 위치의 로봇은 항상 삭제된다.
-            R[-1]=0
+        for i in range(N-2, -1, -1):
+            if ROBOT[i] and not ROBOT[i+1] and BELT[i+1]:
+                ROBOT[i], ROBOT[i+1] = ROBOT[i+1], ROBOT[i]
+                BELT[i+1] -= 1
+                EMPTY[GET] += (BELT[i+1] == 0)
 
-        # 첫번째 자리에 로봇 적재 시도
-        if R[0]==0 and C[0]>=1:
-            R[0] = 1
-            C[0] -= 1
-            k += [0,1][C[0] == 0]  
-        
-        answer += 1
-        if k >= K: break 
-            
-    print(answer)
+        ROBOT[-1] = 0
 
-solution()
+        if not ROBOT[0] and BELT[0]:
+            ROBOT[0] = 1
+            BELT[0] -= 1
+            EMPTY[GET] += (BELT[0] == 0)
+
+        if EMPTY[GET] >= K:
+            return answer
+
+print(solution())

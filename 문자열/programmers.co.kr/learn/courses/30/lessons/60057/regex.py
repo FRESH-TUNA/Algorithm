e1b7fc5f21e6856f599338218a4276a00cef045e
Dongwon Kim <freshtuna@kakao.com>
2022-07-18 19:23:25 +0900

    프로그래머스 60057, 문자열압축, 정규식, 개선된풀이

--- 문자열/programmers.co.kr/learn/courses/30/lessons/60057/regex.py
+++ 문자열/programmers.co.kr/learn/courses/30/lessons/60057/regex.py
@@ -2,17 +2,13 @@ import re
 
 def solution(s):
     def compress(s, length):
-        regex = re.compile(f'([a-z]{{{length}}})\\1*')
-        last_match = None
-        result = ""
-
-        for match in regex.finditer(s):
-            last_match = match
-            start, end = match.span()
-            i = (end-start) // length
-            result += ([str(i), ""][i == 1] + length*"*")
+        REGEX = re.compile(f'([a-z]{{{length}}})\\1*')
+        answer = 0
         
-        return (len(result) + len(s) - last_match.span()[1])
+        for match in re.finditer(REGEX, s):
+            start, end = match.span()
+            count = (end-start) // length    
+            answer += [len(str(count)) , 0][count == 1] + length
+        return answer + len(s) % length
 
-    if len(s) == 1: return 1
-    return min(len(s), min(compress(s, l) for l in range(1, len(s)//2+1)))
+    return min(compress(s, i) for i in range(1, len(s) // 2 + 2))

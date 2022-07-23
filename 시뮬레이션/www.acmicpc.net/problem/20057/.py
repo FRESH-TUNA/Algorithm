705a6ff9489295a97f3e547935671c8844a241a3
Dongwon Kim <freshtuna@kakao.com>
2022-07-23 14:33:42 +0900

    백준 20057, 마법사 상어와 토네이도 ,  구현, 복습

--- 시뮬레이션/www.acmicpc.net/problem/20057/.py
+++ 시뮬레이션/www.acmicpc.net/problem/20057/.py
@@ -1,50 +1,51 @@
+import sys
+
 def solution():
+    input = sys.stdin.readline
     N = int(input())
-    sand = [list(map(int, input().split())) for _ in range(N)]
+    G = [list(map(int, input().split())) for _ in range(N)]
+    ALPHA = 0
+    LEFT = ((-2,0,0.02), (2,0,0.02),
+            (-1,-1,0.1), (1,-1,0.1),
+            (-1,1,0.01), (1,1,0.01),
+            (-1,0,0.07), (1,0,0.07),
+            (0,-2,0.05), (0,-1,ALPHA))
+    DR, DC, DN = (0,1,0,-1), (-1,0,1,0), 4
+    SP = (tuple((r,c,p) for r,c,p in LEFT),
+          tuple((-c,r,p) for r,c,p in LEFT),
+          tuple((r,-c,p) for r,c,p in LEFT),
+          tuple((c,r,p) for r,c,p in LEFT))
 
-    LEFT = [(1, 1, 0.01), (-1, 1, 0.01), 
-            (1, 0, 0.07), (-1, 0, 0.07), 
-            (1, -1, 0.1), (-1, -1, 0.1), 
-            (2, 0, 0.02), (-2, 0, 0.02), (0, -2, 0.05), 
-            (0, -1, 0)]
-    RIGHT = [(x, -y, z) for x,y,z in LEFT]
-    DOWN = [(-y, x, z) for x,y,z in LEFT]
-    UP = [(y, x, z) for x,y,z in LEFT]
-    DISTRIBUTE = [LEFT, DOWN, RIGHT, UP]
+    def distribute(r, c, d):
+        outbound, distributed = 0, 0
+        
+        for dr, dc, p in SP[d]:
+            cr, cc = r+dr, c+dc
 
-    r, c, time = N//2, N//2, 0
-    out, DR, DC, DN, GET = [0], [0,1,0,-1], [-1,0,1,0], 4, 0
-
-    # 모래 계산하는 함수
-    def distribute(r, c, direction):
-        if c < 0: return
-
-        distributed = 0
-        for dx, dy, z in direction:
-            nx = r + dx
-            ny = c + dy
-            
-            # alpha
-            if z == 0:
-                new_sand = sand[r][c] - distributed
+            if p==ALPHA:
+                moved = G[r][c]-distributed
             else:
-                new_sand = int(sand[r][c] * z)
-                distributed += new_sand
+                moved = int(G[r][c]*p)
+                distributed += moved
 
-            if 0<=nx<N and 0<=ny< N:
-                sand[nx][ny] += new_sand
+            if 0<=cr<N and 0<=cc<N:
+                G[cr][cc] += moved
             else:
-                out[GET] += new_sand
+                outbound += moved
+        G[r][c] = 0
+        return outbound
 
-    for i in range(2*N-1):
-        d = i % DN
-        if d == 0 or d == 2:
-            time += 1
-        for _ in range(time):
-            nr = r + DR[d]
-            nc = c + DC[d]
-            distribute(nr, nc, DISTRIBUTE[d])  # y좌표, 방향
-            r, c = nr, nc
-    print(out[GET])
+    def call():
+        r, c, d, move = N//2, N//2, 0, 1
+        outbound = 0
+        
+        while True:
+            for _ in range(move):
+                r, c = r+DR[d], c+DC[d]
+                if c==-1: return outbound
+                outbound += distribute(r, c, d)
+            d = (d+1) % DN
+            if d==0 or d==2: move += 1                
+    print(call())
 
 solution()

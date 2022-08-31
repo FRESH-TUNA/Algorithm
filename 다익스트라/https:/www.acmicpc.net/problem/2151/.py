7fef56f72fb12780ecbcd7c53b95858659f19970
Dongwon Kim <freshtuna@kakao.com>
2022-09-01 00:53:13 +0900

    짜증나는 2151 백준 성공, 다익스트라

--- 다익스트라/https:/www.acmicpc.net/problem/2151/.py
+++ 다익스트라/https:/www.acmicpc.net/problem/2151/.py
@@ -5,37 +5,30 @@ N = int(input())
 G = [list(input().rstrip()) for _ in range(N)]
 D, DR, DC, MAX_ANSWER = 4, (-1,0,1,0), (0,1,0,-1), N*N+N
 DB = [[[MAX_ANSWER]*D for _ in range(N)] for _ in range(N)]
-
 doors, DOOR, DOOR_MAX, GLASS, WALL = [], "#", 2, "!", "*"
 
 def findDoors():
     for i in range(N):
         for j in range(N):
             if G[i][j] == DOOR:
-                for d in range(D):
-                    nr, nc = i+DR[d], j+DC[d]
-                    if nr!=-1 and nr!=N and nc!=-1 and nc!=N and G[nr][nc]!=WALL:
-                        doors.append((0,i,j,d))
-                        break
+                doors.append((0,i,j))
             if len(doors) == DOOR_MAX: return
 
 def answer():
-    sans, sr, sc, sd = doors[0]
-    dans, dr, dc, dd = doors[1]
+    sans, sr, sc = doors[0]
+    dans, dr, dc = doors[1]
     DB[sr][sc] = [0,0,0,0]
-    q = []
-    q.append((0, sr, sc, sd))
+    q = [(0, sr, sc, d) for d in range(D)]
     
     while q:
         ans, r, c, d = heapq.heappop(q)
-        if r==dr and c==dc:
-            break
-        
+
         if DB[r][c][d] < ans:
             continue
 
         nr, nc = r+DR[d], c+DC[d]
-        if nr!=-1 and nr!=N and nc!=-1 and nc!=N and ans<DB[nr][nc][d] and G[nr][nc]!=WALL:
+        if nr!=-1 and nr!=N and nc!=-1 and nc!=N \
+            and ans<DB[nr][nc][d] and G[nr][nc]!=WALL:
             DB[nr][nc][d] = ans
             heapq.heappush(q, (ans, nr, nc, d))
 
@@ -44,12 +37,9 @@ def answer():
 
         for nd in range(D):
             nr, nc = r+DR[nd], c+DC[nd]
+
             if nr==-1 or nr==N or nc==-1 or nc==N or G[nr][nc]==WALL:
                 continue
-            # if DB[nr][nc][nd] != MAX_ANSWER and ans >= DB[nr][nc][nd]:
-            #     DB[nr][nc][nd] = ans
-            #     heapq.heappush(q, (ans, nr, nc, nd))
-            #     continue
             if (ans+1) >= DB[nr][nc][nd]:
                 continue
             DB[nr][nc][nd] = ans+1
